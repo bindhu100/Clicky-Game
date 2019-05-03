8811ef5aa98e3102570d4a6ccc5c59861f999fc3
Bindhu <biji_100@yahoo.com>
2019-05-03 12:44:15 -0600

    update Favicon

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -11,35 +11,7 @@ const Card = props => (
  
 );
 
-// <!--Notes Modal-->
 
-// eslint-disable-next-line no-unused-expressions
-{/* <div id="noteModal" class="modal fade modalNote"  tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div id="notehead" class="modal-header">
-  
-        <h4 class="modal-title">Notes for </h4>
-      </div>
-      <div id="notebody" class="modal-body"> */}
-        {/* ${this.state.score}
-        ${this.state.highscore}
-      */}
-     
-        
-       
-    
-      
-//         <div class="form-group">
-        
-//         </div>
 
-      
-
-//         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
 
 export default Card;
